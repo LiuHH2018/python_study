0367bd63a188bf89abaaaa841e4e7d8fcc46a566
LiuHH2018 <37245266+LiuHH2018@users.noreply.github.com>
2018-05-09 23:37:12 +0800

    D002.py
    阶乘

---
+++ Python游戏攻略/D002.py
@@ -0,0 +1,20 @@
+# Q002
+# Tag: recursion
+# Description: Write a program which can compute the factorial of a given numbers.
+#              The results should be printed in a comma-separated sequence on a single line.
+#              Suppose the following input is supplied to the program:
+#
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+
+
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n - 1)
+
+n = int(input("Pelase input a number:"))
+print(fact(n))
+ 
