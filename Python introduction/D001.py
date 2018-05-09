89b00a7e9859b18a9b0645e0ae876b49180846ee
LiuHH2018 <37245266+LiuHH2018@users.noreply.github.com>
2018-05-09 23:45:40 +0800

    join，for

--- Python introduction/D001.py
+++ Python introduction/D001.py
@@ -1,5 +1,5 @@
 # Q001
-# Tag: Range
+# Tag: Range（join，for）
 # Description: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 #              between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated
 #              sequence on a single line.
