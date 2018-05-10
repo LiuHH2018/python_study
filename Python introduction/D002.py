ae88ea774115577fc04fccb4e7463228d243b7ac
LiuHH2018 <37245266+LiuHH2018@users.noreply.github.com>
2018-05-10 12:53:14 +0800

    input,factorial

--- Python introduction/D002.py
+++ Python introduction/D002.py
@@ -1,5 +1,7 @@
 # Q002
-# Tag: recursion(input,factorial)
+# 16/02/2017, 21:16
+# Tag: recursion
+# Tag 1: input,factorial
 # Description: Write a program which can compute the factorial of a given numbers.
 #              The results should be printed in a comma-separated sequence on a single line.
 #              Suppose the following input is supplied to the program:
