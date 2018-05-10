557ad559110674165a7949fd426849c062db818a
LiuHH2018 <37245266+LiuHH2018@users.noreply.github.com>
2018-05-11 00:33:44 +0800

    dictionary

---
+++ Python introduction/D003.py
@@ -0,0 +1,19 @@
+# Q003
+# 11/05/2018
+# Tag: dictionary
+# Description: With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such
+#              that is an integral number between 1 and n (both included). and then the program should print the
+#              dictionary.
+#
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+i = int(input("Please give a number:"))
+d = dict()
+
+for x in range(1,i+1):
+    d[x] = x*x
+    
+print(d)
